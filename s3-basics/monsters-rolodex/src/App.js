69a80e211d597bff71af7c18174b924eb308d312
Jason Yang <54336962+jasonwithcoffee@users.noreply.github.com>
2022-12-19 14:06:24 -0800

    components

--- s3-basics/monsters-rolodex/src/App.js
+++ s3-basics/monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import logo from './logo.svg';
+import CardList from './components/card-list/card-list.component'
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -25,31 +27,33 @@ class App extends Component {
       );
   }
 
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField }
+    });
+
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
   return (
     <div className="App">
       <input 
       className='search-box' 
       type='search' 
       placeholder='search monsters' 
-      onChange={(event) => {
-        console.log(event.target.value);
-        const searchString = event.target.value.toLocaleLowerCase();
-        const filteredMonsters = this.state.monsters.filter((monster) => {
-          return monster.name.toLocaleLowerCase().includes(searchString);
-        })
+      onChange={ onSearchChange
+      }></input>
 
-        this.setState(() => {
-          return { monsters: filteredMonsters}
-        })
-      }}></input>
-      {
-        this.state.monsters.map((monster) => {
-          return <div key={monster.id}><h1>{monster.name}</h1></div>;
-        })
-      }
-
+    <CardList monsters={filteredMonsters} />
     </div>
   );
   }
